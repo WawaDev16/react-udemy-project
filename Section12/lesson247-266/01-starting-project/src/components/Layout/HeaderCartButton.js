79ca93ebfff6adac9e8bafd5c73b85a85f0932f3
WawaDev16 <wawa.dev@gmail.com>
2023-12-14 22:40:45 +0800

    lesson261-266

--- Section12/lesson247-266/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ Section12/lesson247-266/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,29 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart.context";
-import classes from './HeaderCartButton.module.css'
+import classes from "./HeaderCartButton.module.css";
 
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-const HeaderCartButton = props =>{
-   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-   const  numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-   }, 0);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
 
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
         <CartIcon />
-        </span>
-        <span>your Cart</span>
-        <span className={classes.badge}>
-            {numberOfCartItems}
-        </span>
+      </span>
+      <span>your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-    );
+  );
 };
 
 export default HeaderCartButton;
