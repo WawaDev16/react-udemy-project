cbbcbfc56dd1f55ecf17edea0ae564e0d850e8b9
WawaDev16 <wawa.dev@gmail.com>
2024-01-31 11:51:57 +0800

    lesson305-320

---
+++ Section20/01-starting-project/src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "My first book i ever wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My 2nd Book",
+    description: "My 2nd book i ever wrote",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
